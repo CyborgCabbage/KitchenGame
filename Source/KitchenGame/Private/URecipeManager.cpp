58cca61844d673cb9cd486ac13a02e822183ebf2
CyborgCabbage <finlaylaidlaw9@gmail.com>
2024-02-19 17:41:08 +0000

    Data driven recipe framework

--- Source/KitchenGame/Private/URecipeManager.cpp
+++ Source/KitchenGame/Private/URecipeManager.cpp
@@ -10,3 +10,31 @@ URecipeManager::URecipeManager()
 URecipeManager::~URecipeManager()
 {
 }
+
+void URecipeManager::RegisterIngredient(FString ingredientId, TSubclassOf<AActor> ingredientClass)
+{
+	IdToClass.Add({ ingredientId, ingredientClass });
+}
+
+TSubclassOf<AActor> URecipeManager::TryChop(FString ingredientId)
+{
+	if (!IsValid(ChopTable)) {
+		return nullptr;
+	}
+	TArray<FCookingRecipe*> rows;
+	ChopTable->GetAllRows<FCookingRecipe>("Get Rows for Try Chop", rows);
+	for (auto* row : rows) {
+		if (row->RecipeInput.Num() == 0) {
+			UE_LOG(LogTemp, Warning, TEXT("Chop recipe did not have input"));
+			continue;
+		}
+		if (row->RecipeInput.Num() == 0) {
+			UE_LOG(LogTemp, Warning, TEXT("Chop recipe did not have output"));
+			continue;
+		}
+		if (row->RecipeInput[0] == ingredientId) {
+			return IdToClass.FindChecked(row->RecipeOutput[0]);
+		}
+	}
+	return nullptr;
+}
