be70ee4f292ad86db964638a0699f4b3294ae143
CyborgCabbage <finlaylaidlaw9@gmail.com>
2024-02-21 16:51:30 +0000

    Add c++ base class for ingredient so that it can be accessed by recipe manager

---
+++ Source/KitchenGame/Public/AIngredient.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AIngredient.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class KITCHENGAME_API AIngredient : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AIngredient();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	/** How well done this ingredient is, a value over 1 means burnt */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double Doneness;
+
+	/** How well fried this is, a value over 1 means over-fried */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double Friedness;
+
+	/** How long it takes to go from 0 to 1 in doneness */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double DoneTime;
+
+	/** How long it takes to burn */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double BurnTime;
+
+	/** How long it takes to go from 0 to 1 in friedness */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double FryTime;
+
+	/** How long it takes to over-fry */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	double OverFryTime;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
