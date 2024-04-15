c6550ac13883044a855bac1b3e2ac696befba09b
CyborgCabbage <finlaylaidlaw9@gmail.com>
2024-04-15 14:45:28 +0100

    The CPPening

---
+++ Source/KitchenGame/Private/LockPointTrigger.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LockPointTrigger.h"
+
+// Sets default values
+ALockPointTrigger::ALockPointTrigger() : VisibilityTimer(0.0f)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALockPointTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALockPointTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	VisibilityTimer -= DeltaTime;
+	if(VisibilityTimer < 0.0f) {
+		VisibilityTimer = 0.0f;
+	}
+	Visual->SetVisibility(VisibilityTimer > 0);
+}
+
+void ALockPointTrigger::SetEnableVisual() {
+	VisibilityTimer = 0.2f;
+}
+
---
+++ Source/KitchenGame/Public/Grabbable.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LockPoint.h"
+#include "Grabbable.generated.h"
+
+class UGrabber;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KITCHENGAME_API UGrabbable : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabbable();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/** Please add a function description */
+	UFUNCTION(BlueprintCallable)
+	void ConfigureGrabbable(UPrimitiveComponent* Physics, bool InHand, USceneComponent* BottomPoint, USceneComponent* GrabPoint);
+
+	/** Please add a function description */
+	UFUNCTION(BlueprintCallable)
+	void SetEnablePhysics(bool Simulating, bool Colliding);
+
+	/** Please add a function description */
+	UFUNCTION(BlueprintCallable)
+	void UnlockIfLocked();
+
+	/** Please add a function description */
+	UFUNCTION(BlueprintCallable)
+	void SetEnableCollision(bool Value, bool Recursive);
+
+	/** Please add a function description */
+	UFUNCTION(BlueprintCallable)
+	void SetCanGrab(bool Value);
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	TObjectPtr<UPrimitiveComponent> GrabTarget;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	bool InHand;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	TObjectPtr<USceneComponent> BottomPoint;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	TObjectPtr<USceneComponent> GrabPoint;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	bool IsLocked;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	TObjectPtr<ULockPoint> LockPoint;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	bool IsGrabbed;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditInstanceOnly, Category = "Default")
+	TObjectPtr<AActor> Grabber;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Default")
+	bool CanGrab;
+
+};
